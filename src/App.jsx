d6849c52108fd7eceaccd8775c46aac0d531d4d1
raniyaptla <141708870+raniyaptla@users.noreply.github.com>
2024-01-20 12:47:26 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,22 @@
-import './App.css';
-import lake from "./image/lake.JPG";
-import GallaryFooter from './components/GallaryFooter';
 
-const imageData = [
-  {
-    id:1,
-    img:lake 
-  },
-  {
-    id:2,
-    img:lake
-  },
-  {
-    id:3,
-    img:lake
-  },
-  {
-    id:4,
-    img:lake
-  }
-]
+import './App.css';
+import GallaryFooter from './components/GallaryFooter';
+import GalleryHeader from './components/GalleryHeader';
+import GalleryBody from './components/GalleryBody';
+import Data from './components/Data';
+
+
+
 
 function App() {
   return (
     <div>
-      <header>
-        <img className="logo" src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt=""></img>
-        <nav>
-          <ul className='nav_links'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About</a></li>
-            <li><a href='#'>More</a></li>
-          </ul>
-
-        </nav>
-        <a className='cta' href='#'><button>Contact</button></a>
-      </header>
+      <GalleryHeader/>
+      
 
       <div>
-        {
-          imageData.map(image=>{
-            return(
-              <div key={image.id} className="column">
-                <img src={image.img} alt=""></img>
-              </div>
-            )
-          })
-        }
+       <GalleryBody data={Data}/>
+        
       </div>
       {/* adding footer component */}
       <GallaryFooter/> 
@@ -55,3 +25,4 @@ function App() {
 }
 
 export default App;
+// star
